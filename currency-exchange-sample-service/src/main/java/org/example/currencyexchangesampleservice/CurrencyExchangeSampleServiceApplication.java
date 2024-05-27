f7fceb7ce1e76e89da59375fd1632e9e8660e220
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-05-27 21:59:16 +0700

    init-prj

---
+++ currency-exchange-sample-service/src/main/java/org/example/currencyexchangesampleservice/CurrencyExchangeSampleServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.currencyexchangesampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeSampleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeSampleServiceApplication.class, args);
+    }
+
+}
